af7a3a04b99a7d98d5fbbdc3c09c5ebcf249b71a
Roland Ukor <rolandukor@gmail.com>
2015-06-16 21:56:29 +0100

    Initial commit

---
+++ src/com/firstlinq/om_ssr_node.clj
@@ -0,0 +1,6 @@
+(ns com.firstlinq.om-ssr-node)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/com/firstlinq/om_ssr_node_test.clj
@@ -0,0 +1,7 @@
+(ns com.firstlinq.om-ssr-node-test
+  (:require [clojure.test :refer :all]
+            [com.firstlinq.om-ssr-node :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
