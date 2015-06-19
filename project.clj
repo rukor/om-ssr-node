2e3ca4fd154ea1dacda055e6f54017c791f93875
Roland Ukor <rolandukor@gmail.com>
2015-06-19 14:34:23 +0100

    Change URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.firstlinq/om-ssr-node "0.1.1-SNAPSHOT"
             :description "OM Server Side Rendering for NodeJS"
-            :url "http://bitbucket.org/rukor/om-ssr-node"
+            :url "http://github.com/rukor/om-ssr-node"
             :license {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.7.0-RC1"]
